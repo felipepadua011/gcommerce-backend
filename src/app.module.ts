3f6b295d78146514cc10ae8edc5e89683c408427
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-01-19 22:47:29 -0300

    app.module com produto.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsuarioModule } from "./usuario/usuario.module";
+import { ProdutoModule } from "./produto/produto.module";
 
 @Module({
-  imports: [UsuarioModule, PrismaModule],
+  imports: [UsuarioModule, PrismaModule, ProdutoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { Produto } from '@prisma/client'
-import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateProdutoDto } from './dto/create-produto.dto';
-import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { Injectable } from "@nestjs/common";
+import { Produto } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateProdutoDto } from "./dto/create-produto.dto";
+import { UpdateProdutoDto } from "./dto/update-produto.dto";
 
 @Injectable()
 export class ProdutoService {
-
   constructor(private prisma: PrismaService) {}
 
   async createPrisma(createProdutoDto: CreateProdutoDto): Promise<Produto> {
@@ -20,17 +19,20 @@ export class ProdutoService {
   }
 
   async findOnePrisma(id: number): Promise<Produto> {
-    return await this.prisma.produto.findUnique({ where: {id}});
+    return await this.prisma.produto.findUnique({ where: { id } });
   }
 
-  async updatePrisma(id: number, _updateProdutoDto: UpdateProdutoDto): Promise<Produto> {
-    return  await this.prisma.produto.update({
-      data:{..._updateProdutoDto},
-      where:{id}
-  });
+  async updatePrisma(
+    id: number,
+    _updateProdutoDto: UpdateProdutoDto
+  ): Promise<Produto> {
+    return await this.prisma.produto.update({
+      data: { ..._updateProdutoDto },
+      where: { id },
+    });
   }
 
   async removePrisma(id: number) {
-    return await this.prisma.produto.delete({ where: {id}});
+    return await this.prisma.produto.delete({ where: { id } });
   }
 }
