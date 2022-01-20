d3763683fa3e5e9abe4dc09915e78e7be17dfc22
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-01-19 21:48:09 -0300

    auth completo

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,26 +1,79 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { LoginDto } from "./../auth/dto/login.dto";
+import { UpdateUsuarioDto } from "./dto/update-usuario.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import { Prisma, Usuario } from "@prisma/client";
+import * as bcrypt from "bcrypt";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { JwtPayload } from "src/auth/jwt.strategy";
 
 @Injectable()
 export class UsuarioService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.UsuarioCreateInput): Promise<Usuario> {
+    data.senha = await bcrypt.hash(data.senha, 10);
+    try {
+      const createdUser = await this.prisma.usuario.create({ data });
+      createdUser.senha = undefined;
+      return createdUser;
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        "Verifique os dados e tente novamente.",
+        HttpStatus.BAD_REQUEST
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all usuario`;
+  async findByLogin(login: LoginDto): Promise<Usuario> {
+    const user = await this.prisma.usuario.findFirst({
+      where: {
+        email: login.email,
+      },
+    });
+
+    const senhaIgual = await bcrypt.compare(login.senha, user.senha);
+
+    if (!user) {
+      throw new HttpException("Usuário não encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    if (!senhaIgual) {
+      throw new HttpException("Senha inválida.", HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} usuario`;
+  async validateUser(payload: JwtPayload): Promise<Usuario> {
+    const user = await this.prisma.usuario.findFirst({
+      where: {
+        email: payload.email,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException("Token inválido.", HttpStatus.UNAUTHORIZED);
+    }
+
+    return user;
   }
 
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
+  async findAll(): Promise<Usuario[]> {
+    return await this.prisma.usuario.findMany();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} usuario`;
+  async findOne(id: number): Promise<Usuario> {
+    return await this.prisma.usuario.findUnique({ where: { id } });
+  }
+
+  async update(id: number, data: UpdateUsuarioDto): Promise<Usuario> {
+    data.senha = await bcrypt.hash(data.senha, 10);
+    return await this.prisma.usuario.update({ data, where: { id } });
+  }
+
+  async remove(id: number): Promise<Usuario> {
+    return await this.prisma.usuario.delete({ where: { id } });
   }
 }
