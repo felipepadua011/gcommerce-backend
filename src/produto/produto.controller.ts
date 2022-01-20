288ef1d269975522bafe6e16b075282235229ccd
felipepadua011 <89053801+felipepadua011@users.noreply.github.com>
2022-01-19 22:02:15 -0300

    crud produtos

---
+++ src/produto/produto.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { ProdutoService } from './produto.service';
+import { CreateProdutoDto } from './dto/create-produto.dto';
+import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('produto')
+export class ProdutoController {
+  constructor(private readonly produtoService: ProdutoService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  create(@Body() createProdutoDto: CreateProdutoDto) {
+    return this.produtoService.createPrisma(createProdutoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.produtoService.findAllPrisma();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.produtoService.findOnePrisma(+id);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
+  update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
+    return this.produtoService.updatePrisma(+id, updateProdutoDto);
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
+  remove(@Param('id') id: string) {
+    return this.produtoService.removePrisma(+id);
+  }
+}
