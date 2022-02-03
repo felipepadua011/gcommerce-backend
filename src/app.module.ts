ee5087c192ca3169561ae1ae1f56edce45699b12
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-02-02 22:53:31 -0300

    consegui converter excel para json localmente

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ProdutoModule } from "./produto/produto.module";
 import { AuthModule } from "./auth/auth.module";
 import { ProdutosprecosModule } from "./produtosprecos/produtosprecos.module";
 import { RoleModule } from './role/role.module';
+import { MassaModule } from './massa/massa.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { RoleModule } from './role/role.module';
     AuthModule,
     ProdutosprecosModule,
     RoleModule,
+    MassaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
