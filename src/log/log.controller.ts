1d929348f9322e0cbe4152c9e2de851d9bb2defc
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-02-20 18:16:50 -0300

    att com log funcionando no botao editar

---
+++ src/log/log.controller.ts
@@ -0,0 +1,28 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    UseGuards,
+  } from "@nestjs/common";
+  import { LogService } from "./log.service"; 
+  import { AuthGuard } from "@nestjs/passport";
+  import { CreateLogDto} from "./dto/create-log.dto";
+
+@Controller('log')
+export class LogController {
+    constructor(private readonly logService: LogService) {}
+
+
+  @Post()
+  @UseGuards(AuthGuard("jwt"))
+  createLog(@Body() createLogDto: CreateLogDto) {
+    return this.logService.createLog(createLogDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard("jwt"))
+  getLog() {
+    return this.logService.getLog();
+  }
+}
